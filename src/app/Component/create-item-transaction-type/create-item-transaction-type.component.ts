8cc28613634cd2a600cad43c1ae6d88eb42009d9
IFTEKHAR AHAMED SIDDIQUEE <iftekharahamed56@gmail.com>
2024-03-07 10:30:01 +0600

    Create item transaction type

--- src/app/Component/create-item-transaction-type/create-item-transaction-type.component.ts
+++ src/app/Component/create-item-transaction-type/create-item-transaction-type.component.ts
@@ -1,12 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { APIService } from '../../api.service';
+import { PopupComponent } from '../popup/popup.component';
+interface transactionType {
+  id: number,
+  transactionName: string,
+  isActive: boolean
+}
 @Component({
   selector: 'app-create-item-transaction-type',
   templateUrl: './create-item-transaction-type.component.html',
   styleUrl: './create-item-transaction-type.component.css',
   standalone: true,
-  imports: []
+  imports: [CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class CreateItemTransactionTypeComponent {
+  itemTransactionTypeFrom!: FormGroup;
 
+  constructor(private fb: FormBuilder, private apiService: APIService, private dialog: MatDialog) { }
+
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+  initializeForm(): void {
+    this.itemTransactionTypeFrom = this.fb.group({
+      id: [0],
+      transactionName: ['', Validators.required],
+      isActive: [true, Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    const url = "/User/CreateItemTransactionType";
+    const formData: transactionType = this.itemTransactionTypeFrom.value;
+    this.apiService.post(formData, url).subscribe(res => {
+      this.openPopup(res.message, "Ok", "https://cdn-icons-png.flaticon.com/512/190/190411.png");
+      this.initializeForm();
+    }, error => {
+      this.openPopup("Faild to Create", "Try Again", "https://cdn-icons-png.flaticon.com/512/1047/1047711.png");
+    });
+  }
+  openPopup(message: string, action: string, imgsource: string): void {
+    const dialogRef = this.dialog.open(PopupComponent, {
+      data: { message, action, imgsource },
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      console.log('Popup closed');
+    });
+  }
 }
